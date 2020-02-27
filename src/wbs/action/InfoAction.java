260b2d12b8beef9649b4e9b5758cad4ee71a48e0
koitt04 <koitt04@USER_PC>
2020-02-27 19:23:58 +0900

    추가

--- src/wbs/action/InfoAction.java
+++ src/wbs/action/InfoAction.java
@@ -3,25 +3,14 @@ package wbs.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import mybatis.dao.MemDAO;
-import mybatis.vo.MemVO;
-
-public class IdAction implements Action {
+public class InfoAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		// 아이디 유무
+		// TODO Auto-generated method stub
 		String id = request.getParameter("m_id");
 		
-		
-		
-		MemVO vo = MemDAO.getMember(id);
-		
-		if(vo != null)
-			request.setAttribute("vo_id", id);
-		
-		
-		return "/idCheck.jsp";
+		return "/Memview.jsp";
 	}
 
 }
